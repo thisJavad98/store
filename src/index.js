d3619496a9c74a293f9e6667b8cd39bb0630ab4a
MohamadJavadMoghofei <81424606+javad30011377@users.noreply.github.com>
2021-06-18 18:36:55 +0430

    seting folders!!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Container, Jumbotron, Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const element=
+<>
+  <div className='d-flex  justify-content-center'>
+    <h1 className='display-4'>Welcome to javad shoping </h1>
+  </div>
+  
+</>
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
