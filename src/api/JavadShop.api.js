2ad9142f86cad09ea869c7352e4ad154825229de
MohamadJavadMoghofei <81424606+javad30011377@users.noreply.github.com>
2021-06-22 17:04:07 +0430

    Create api and Install the redux and axios

---
+++ src/api/JavadShop.api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { response } from "express";
+
+const BASE_URL ='http://localhost:5000';
+const httpApi=axios.create();
+
+httpApi.defaults.baseURL = BASE_URL;
+
+export function Products(){
+
+    return httpApi.get(`/products`)
+        .then((response)=> response.data)
+        .catch((error)=> console.log(error))
+
+}
