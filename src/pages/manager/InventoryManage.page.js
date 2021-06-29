f50988696851623292c3b7e335a328564a19f30d
MohamadJavadMoghofei <81424606+javad30011377@users.noreply.github.com>
2021-06-30 00:24:00 +0430

    set pagination of invertory panel

--- src/pages/manager/InventoryManage.page.js
+++ src/pages/manager/InventoryManage.page.js
@@ -1,128 +1,94 @@
 import React from "react";
-import {Component} from "react";
-import { Table , Button, Pagination, PaginationLink,PaginationItem } from 'reactstrap';
+import { Component } from "react";
+import {
+  Table,
+  Button,
+  Pagination,
+  PaginationLink,
+  PaginationItem,
+} from "reactstrap";
 import { getProducts } from "../../api/JavadShop.api";
+import CustomPagination from "../../components/Pagination.component";
 
-class QuantityPanel extends Component{
-    state={
-        products:[]
-    }
-    async componentDidMount(){
-        this.setState({products:await getProducts()})
-        console.log(this.state.products.map((item)=>{console.log(item.name)}))
-        console.log(this.state.products)
-
-    }
-    render(){
-        return(
-            <>
-                <div className='d-flex justify-content-between m-5 '>
-                    <div>
-                        <Button color='success' className='ps-4 pe-4 p-2'>
-                            ذخیره
-                        </Button>
-                    </div>
-                    <>
-                        <h3>مدیریت موجودی و قیمت ها</h3>
-                    </>
-                </div>
-                <div className='m-5'>
-                <Table bordered striped dir='rtl'>
-                    <thead>
-                        <tr>
-                            <th> کالا </th>
-                            <th> قیمت </th>
-                            <th> موجودی </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.products.map(item=>{
-                            return(
-                                <tr key={item.id}>
-                                    <td>{item.name}</td>
-                                    <td>{item.price}</td>
-                                    <td>{item.inventory}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                    {/* <tbody>
-                        <tr>
-                            <td>کره شکلی سنتی-۱۰۰ گرمی</td>
-                            <th> ۱۰.۰۰۰ </th>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                        <tr>
-                            <td>لوبیا قرمز-۹۰۰ گرمی</td>
-                            <td> ۱۰.۰۰۰ </td>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                        <tr>
-                            <td>روغن سرخ کردنی-۱.۳۵ کیلویی</td>
-                            <th> ۱۰.۰۰۰ </th>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                        <tr>
-                            <td>کره شکلی سنتی-۱۰۰ گرمی</td>
-                            <th> ۱۰.۰۰۰ </th>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                        <tr>
-                            <td>روغن سرخ کردنی-۱.۳۵ کیلویی</td>
-                            <th> ۱۰.۰۰۰ </th>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                        <tr>
-                            <td>کره شکلی سنتی-۱۰۰ گرمی</td>
-                            <th> ۱۰.۰۰۰ </th>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                        <tr>
-                            <td>لوبیا قرمز-۹۰۰ گرمی</td>
-                            <td> ۱۰.۰۰۰ </td>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                        <tr>
-                            <td>روغن سرخ کردنی-۱.۳۵ کیلویی</td>
-                            <th> ۱۰.۰۰۰ </th>
-                            <th> ۲۰۰ </th>
-                        </tr>
-                    </tbody> */}
-                </Table>
-                <div className='d-flex justify-content-center mt-4'>
-                    <Pagination aria-label="Page navigation example">
-                        <PaginationItem disabled>
-                            <PaginationLink first href="#" />
-                        </PaginationItem>
-                        <PaginationItem disabled>
-                            <PaginationLink previous href="#" />
-                        </PaginationItem>
-                        <PaginationItem active>
-                            <PaginationLink href="#">
-                            1
-                            </PaginationLink>
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink href="#">
-                            2
-                            </PaginationLink>
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink href="#">
-                            3
-                            </PaginationLink>
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink next href="#" />
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink last href="#" />
-                        </PaginationItem>
-                        </Pagination>
-                </div>
-                </div>
-            </>
-        );
-    }
+class QuantityPanel extends Component {
+  state = {
+    products: [],
+    productShow: [],
+    currentPage: 1,
+    productPerPage: 5,
+  };
+  async componentDidMount() {
+    this.setState({ products: await getProducts()});
+    this.setState({...this.state,productShow: this.state.products.slice(0, 5)});
+    console.log(
+      this.state.products.map((item) => {
+        console.log(item.name);
+      })
+    );
+    console.log(this.state.products);
+  }
+  render() {
+    return (
+      <>
+        <div className="d-flex justify-content-between m-5 ">
+          <div>
+            <Button color="success" className="ps-4 pe-4 p-2">
+              ذخیره
+            </Button>
+          </div>
+          <span>
+            <span className="shadow bg-warning p-1 ps-3 pe-3 rounded text-secondary">
+              ( {this.state.currentPage}/
+              {Math.floor(this.state.products.length / 5) + 1} )
+            </span>
+          </span>
+          <>
+            <h3>مدیریت موجودی و قیمت ها</h3>
+          </>
+        </div>
+        <div className="m-5">
+          <Table bordered striped dir="rtl">
+            <thead>
+              <tr>
+                <th> کالا </th>
+                <th> قیمت </th>
+                <th> موجودی </th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.productShow.map((item) => {
+                return (
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td>{item.inventory}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <div className="d-flex justify-content-center mt-4">
+            <CustomPagination
+              perPage={this.state.productPerPage}
+              totalPage={this.state.products.length}
+              paginate={(pageNumber) =>
+                this.setState({
+                  ...this.state,
+                  currentPage: pageNumber,
+                  indexOfLastProduct: pageNumber * this.state.productPerPage,
+                  indexOfFirstProduct:
+                    pageNumber * this.state.productPerPage - 5,
+                  productShow: this.state.products.slice(
+                    pageNumber * this.state.productPerPage - 5,
+                    pageNumber * this.state.productPerPage
+                  ),
+                })
+              }
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 export default QuantityPanel;
