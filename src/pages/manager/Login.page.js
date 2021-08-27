478dcbb97df126da7db7ef1fbc32e5cb454d3e91
MohamadJavadMoghofei <81424606+javad30011377@users.noreply.github.com>
2021-08-27 17:03:34 +0430

    ser authotication

--- src/pages/manager/Login.page.js
+++ src/pages/manager/Login.page.js
@@ -1,37 +1,81 @@
 import React from "react";
-import {Component} from "react";
-import {  Form, FormGroup, Label, Input , FormText} from "reactstrap";
-import {Link} from 'react-router-dom';
+import { Component } from "react";
+import { Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import { Link, Redirect } from "react-router-dom";
 
-class LoginPage extends Component{
-    render(){
-        return(
-            <div className='vh-100 d-flex justify-content-center align-items-center'>        
-                <Form className= 'p-4 border rounded shadow p-3 mb-5 bg-white rounded' >
-                    <FormText className='d-flex justify-content-center fs-4'>
-                        ورود به پنل مدیریت فروشگاه جواد
-                    </FormText>
-                    <FormGroup className='p-4 pb-0' dir='rtl'>
-                        <Label for='user' className='pb-2'>نام کاربری :</Label>
-                        <Input type='text' id='user'/>
-                    </FormGroup>
-                    <FormGroup className='p-4 pt-3'  dir='rtl'>
-                        <Label for='password' className='pb-2'>رمز عبور :</Label>
-                        <Input type='password' id='password'/>
-                    </FormGroup>
-                    <div className='d-flex justify-content-center'>
-                        <Link className='rounded text-decoration-none text-light ps-4 pe-4 p-2 bg-primary'  to='/panel-product'>ورود</Link>
-                    </div>
-                    <div className='pt-4'>
-                        <Link className='text-decoration-none' to="/">
-                            بازگشت به سایت
-                        </Link>                          
-                    </div>
-                </Form>
+class LoginPage extends Component {
+  state = {
+    userName: "",
+    password: "",
+    page: "login",
+  };
+  render() {
+    return this.state.page === "login" ? (
+      <div className="vh-100 d-flex justify-content-center align-items-center">
+        <Form className="p-4 border rounded shadow p-3 mb-5 bg-white rounded">
+          <FormText className="d-flex justify-content-center fs-4">
+            ورود به پنل مدیریت فروشگاه جواد
+          </FormText>
+          <FormGroup className="p-4 pb-0" dir="rtl">
+            <Label for="user" className="pb-2">
+              نام کاربری :
+            </Label>
+            <Input
+              value={this.state.userName}
+              onChange={(e) => {
+                this.setState({ ...this.state, userName: e.target.value });
+              }}
+              type="text"
+              id="user"
+            />
+          </FormGroup>
+          <FormGroup className="p-4 pt-3" dir="rtl">
+            <Label for="password" className="pb-2">
+              رمز عبور :
+            </Label>
+            <Input
+              value={this.state.password}
+              onChange={(e) => {
+                this.setState({ ...this.state, password: e.target.value });
+              }}
+              type="password"
+              id="password"
+            />
+          </FormGroup>
+          <div
+            onClick={() => {
+              if (
+                this.state.userName === "javad@javad.com" &&
+                this.state.password === "javad3001"
+              ) {
+                this.setState({ ...this.state, page: "panel" });
+              } else alert(" نام کاربری یا رمز عبور صحیح نمی باشد!!");
+            }}
+            className="d-flex justify-content-center"
+          >
+            <div
+              style={{
+                color: "white",
+                padding: "8px 12px",
+                backgroundColor: "#039dfc",
+                cursor: "pointer",
+                borderRadius: "6px",
+              }}
+            >
+              ورود
             </div>
-        )
-    }
-
+          </div>
+          <div className="pt-4">
+            <Link className="text-decoration-none" to="/">
+              بازگشت به سایت
+            </Link>
+          </div>
+        </Form>
+      </div>
+    ) : (
+      <Redirect to="/panel-product" />
+    );
+  }
 }
 
 export default LoginPage;
