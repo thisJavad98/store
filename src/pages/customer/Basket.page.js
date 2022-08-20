81ebe98151fe982146130b4461f713e0d441ca31
mj407382 <mj407382@gmail.com>
2022-08-21 00:56:30 +0430

    update basket page

--- src/pages/customer/Basket.page.js
+++ src/pages/customer/Basket.page.js
@@ -17,64 +17,71 @@ const Basket = (props) => {
           <h3>سبد خرید</h3>
         </div>
         <div className="m-5">
-          <Table striped className="border">
-            <thead>
-              <tr>
-                <th> کالا </th>
-                <th> قیمت </th>
-                <th> تعداد </th>
-                <th> مجموع </th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.basketProduts.map((item, index) => {
-                console.log(item);
-                return (
-                  <tr key={index}>
-                    <th className="col-4">{item.data.name}</th>
-                    <td>{item.data.price}</td>
-                    <td>
-                      <input
-                        className="bg-transparent border-0"
-                        type="number"
-                        value={item.number}
-                        min="1"
-                        onChange={(e) =>
-                          e.target.value <= item.data.inventory
-                            ? props.changeNumberOfOrder(
-                                item.id,
-                                +e.target.value
-                              )
-                            : alert(
-                                `تعداد سفارش شما از موجودی این کالا بیشتر است!!! (تعداد موجودی کالا :${item.data.inventory})`
-                              )
-                        }
-                        max={item.data.inventory}
-                      />
-                    </td>
-                    <td>{item.sumOfPrice}</td>
-                    <td>
-                      <span
-                        type="button"
-                        className="text-primary"
-                        onClick={() => {
-                          props.deleteProductFromBasket(
-                            item.id,
-                            item.sumOfPrice
-                          );
-                          console.log(item.id);
-                        }}
-                      >
-                        {" "}
-                        حذف{" "}
-                      </span>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
+          {props.basketTotalPrice ? (
+            <Table striped className="border">
+              <thead>
+                <tr>
+                  <th> کالا </th>
+                  <th> قیمت </th>
+                  <th> تعداد </th>
+                  <th> مجموع </th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {props.basketProduts.map((item, index) => {
+                  console.log(item);
+                  return (
+                    <tr key={index}>
+                      <th className="col-4">{item.data.name}</th>
+                      <td>{item.data.price}</td>
+                      <td>
+                        <input
+                          className="bg-transparent border-0"
+                          type="number"
+                          value={item.number}
+                          min="1"
+                          onChange={(e) =>
+                            e.target.value <= item.data.inventory
+                              ? props.changeNumberOfOrder(
+                                  item.id,
+                                  +e.target.value
+                                )
+                              : alert(
+                                  `تعداد سفارش شما از موجودی این کالا بیشتر است!!! (تعداد موجودی کالا :${item.data.inventory})`
+                                )
+                          }
+                          max={item.data.inventory}
+                        />
+                      </td>
+                      <td>{item.sumOfPrice}</td>
+                      <td>
+                        <span
+                          type="button"
+                          className="text-primary"
+                          onClick={() => {
+                            props.deleteProductFromBasket(
+                              item.id,
+                              item.sumOfPrice
+                            );
+                            console.log(item.id);
+                          }}
+                        >
+                          {" "}
+                          حذف{" "}
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          ) : (
+            <div style={{ display: "flex", justifyContent: "center"}}>
+              <img height={"300"} src={"/assets/svg/Emptytrabajo.svg"} />
+            </div>
+          )}
+
           <div className="d-flex justify-content-center align-item-center m-5 mt-5 fixed-bottom pb-5">
             <div className="h4 mb-5 ms-5 p-3">
               جمع : {props.basketTotalPrice} تومان
